9f3f916eb8a6b7d53e5e1376ab6892cedae66108
roycao1992 <Roy@Xingguos-MacBook-Pro.local>
2014-11-02 10:28:05 -0800

    final version, sack implemented

--- project2/Sender.py
+++ project2/Sender.py
@@ -4,14 +4,15 @@ import getopt
 import Checksum
 import BasicSender
 
+
 '''
 This is a skeleton sender class. Create a fantastic transport protocol here.
 '''
 class Sender(BasicSender.BasicSender):
     def __init__(self, dest, port, filename, debug=False, sackMode=False):
         super(Sender, self).__init__(dest, port, filename, debug)
-        if sackMode:
-            raise NotImplementedError #remove this line when you implement SACK
+        #if sackMode:
+            #raise NotImplementedError #remove this line when you implement SACK
 
         self.file_read = True
         if self.infile == sys.stdin:
@@ -24,9 +25,12 @@ class Sender(BasicSender.BasicSender):
         self.end_seqno = None
         self.msg_type = None
         self.packet_cached = []
+        self.sack_list = []
+        self.sack_list_copy = []
+        self.sack_retrans = False
+
     def handle_response(self,response_packet):
         if Checksum.validate_checksum(response_packet):
-            print "recv: %s" % response_packet
             received_packet_split = self.split_packet(response_packet)
             return received_packet_split
         else:
@@ -47,65 +51,136 @@ class Sender(BasicSender.BasicSender):
                 self.msg_type = 'end'
             if msg == "done":
                 self.msg_type = 'end'
-        
+
             packet = self.make_packet(self.msg_type, self.seqno, msg)
             self.send(packet)
             self.packet_cached.append(packet)
             self.window.append(i)
-            #print "send: %s" % packet
             if self.msg_type == 'end':
-                if self.end_seqno == None:
+                if self.end_seqno is None:
                     self.end_seqno = self.seqno
                     break
+
+        # record the number of duplicate acks #
         dup_ack_num = 0
+        
         while True:
-            #print self.window
             response = self.receive(self.timeout)
-            if(response == None):
-                print "timeout dude!"
-                self.handle_timeout()
-                dup_ack_num = 0
-                continue
+            if response is None :
+                if self.sack_list == [] and self.sack_retrans is False:
+                    self.handle_timeout()
+                    # reset dup_ack counter after handle a timeout #
+                    dup_ack_num = 0
+                    continue
+                else:
+                    if self.sack_retrans is False:
+                        self.sack_list_copy = self.sack_list[:]
+                        self.handle_sack_timeout()
+                        self.sack_list = []
+                        dup_ack_num = 0
+                    else:
+                        self.handle_sack_timeout()
+                    continue
+
             received_packet = self.handle_response(response)
             
-            #if received packet is invalid, ignore it#
+            # if received packet is invalid, ignore it #
             if(received_packet == None):
                 continue
-
-            ack = int(received_packet[1])
             
-            #check if it is the last seqno received#
-            if(self.end_seqno != None and ack == self.end_seqno + 1):
-                break
-            if ack < self.window[0]:
-                continue
-            if ack == self.window[0]:
-                dup_ack_num += 1
-                if dup_ack_num == 3:
-                    print "dup dude!"
-                    self.handle_dup_ack(ack)
+            if received_packet[0] == "ack":
+                ack = int(received_packet[1])
+                # check if it is the last seqno received #
+                if self.end_seqno is not None and ack == self.end_seqno + 1:
+                    break
+                if ack < self.window[0]:
+                    # if received ack smaller than first elem in window, igore it #
+                    continue
+                if ack == self.window[0]:
+                    dup_ack_num += 1
+                    if dup_ack_num == 3:
+                        self.handle_dup_ack(ack)
+                        # reset dup_ack counter after handle a dup ack #
+                        dup_ack_num = 0
+                    continue
+                else:
+                    self.handle_new_ack(ack)
+                    # reset dup_ack counter after handle a new ack #
                     dup_ack_num = 0
-                continue
+            
+            # below is sack mode #
             else:
-                print "handle_new_ack"
-                self.handle_new_ack(ack)
-                dup_ack_num = 0
+                if received_packet[1] is not None :
+                    cum_ack,rest=self.split_str(received_packet[1])
+                    cum_ack=int(cum_ack)
+                    if self.end_seqno is not None and cum_ack == self.end_seqno + 1:
+                        break
+                    elif rest==[""] :
+                        if cum_ack < self.window[0]:
+                            continue
+                        elif cum_ack == self.window[0]:
+                            continue
+                        else:
+                            self.handle_new_ack(cum_ack)
+                            dup_ack_num = 0
+                            continue
+                    else:
+                        if cum_ack != self.window[0]:
+                            self.handle_new_ack(cum_ack)
+                            dup_ack_num = 0
+                        else:
+                            dup_ack_num += 1
+
+                        if self.sack_list == []:
+                            self.sack_list = self.window[:]
+
+                        for i in rest:
+                            i = int(i)
+                            try:
+                                if self.sack_retrans is False:
+                                    self.sack_list.remove(i)
+                                else:
+                                    self.sack_list_copy.remove(i)
+
+                            except ValueError:
+                                pass
+                        if dup_ack_num == 3:
+                            self.sack_list_copy = self.sack_list[:]
+                            self.handle_sack_timeout()
+                            self.sack_list = []
+                            dup_ack_num = 0
+                            continue
+                        else:
+                            continue
 
 
+    def handle_sack_timeout(self):
+       self.sack_retrans = True
+       offset = self.sack_list_copy[0]
+       for packet_num in self.sack_list_copy:
+           index = packet_num - offset
+           packet = self.packet_cached[index]
+           self.send(packet)
+
+
+    def split_str(self,msg):
+        pieces = msg.split(';')
+        cum_ack, sk = pieces[0:2]
+        sk = sk.split(",") #pieces can be [""], ["2","3"]
+        return cum_ack,sk
 
 
     def handle_timeout(self):
         for packet in self.packet_cached:
             self.send(packet)
-    
-    #print "send: %s" % packet
-    #self.seqno += 1
-
 
 
     def handle_new_ack(self, ack):
+        self.sack_list = []
+        self.sack_retrans = False
+        self.sack_list_copy = []
         move_size = ack - self.window[0]
-        if self.msg_type == 'end':# and (self.end_seqno != None and ack == self.end_seqno + 1) :
+        if self.msg_type == 'end':
             for i in range(1, move_size + 1):
                 self.seqno += 1
                 self.window.pop(0)
@@ -115,7 +190,7 @@ class Sender(BasicSender.BasicSender):
             self.seqno += 1
             self.window.pop(0)
             self.packet_cached.pop(0)
-            
+
             self.window.append(self.seqno)
             #initialize the sending packet
             msg = ""
@@ -131,17 +206,17 @@ class Sender(BasicSender.BasicSender):
                 self.msg_type = 'end'
             if msg == "done":
                 self.msg_type = 'end'
-                
+
             packet = self.make_packet(self.msg_type, self.seqno, msg)
             self.send(packet)
             self.packet_cached.append(packet)
 
+
             #print "send: %s" % packet
             if self.msg_type == 'end':
-                if self.end_seqno == None:
+                if self.end_seqno is None:
                     self.end_seqno = self.seqno
                     return
-                return
 
 
     def handle_dup_ack(self, ack):
